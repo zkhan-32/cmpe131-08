56905568f4751837a2a336587bdbaa741878e9ab
Spartan <spartan@macbookair.mynetworksettings.com>
2025-11-13 18:03:13 -0800

    Feat: Add Docker and CI/CD pipeline

---
+++ my-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
+
